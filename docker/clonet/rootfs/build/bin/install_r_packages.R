4942c223dff43a86544dbb148ae0288795172bb9
Tarcisio <tarcisio.fedrizzi@gmail.com>
2018-05-03 10:08:20 +0200

    Removes igraph 1.1.2 patched install

--- docker/clonet/rootfs/build/bin/install_r_packages.R
+++ docker/clonet/rootfs/build/bin/install_r_packages.R
@@ -1,12 +1,7 @@
 #!/usr/bin/env Rscript
 
-# To be removed when ipgrah is updated to a version > 1.1.2
-igraph_deps  <- c('irlba', 'magrittr', 'Matrix', 'pkgconfig')
-packages     <- c('docopt', 'dgof', 'pso', 'reshape2', 'sets', igraph_deps)
+packages     <- c('docopt', 'dgof', 'pso', 'reshape2', 'sets', 'igraph')
 repository   <- 'https://cloud.r-project.org/'
-download_dir <- file.path(getwd(), 'tmp')
-dir.create(download_dir)
 install.packages(packages, repos = repository)
-download.packages('igraph', download_dir, repos = repository)
 rr         <- as.integer(!all(sapply(packages, function (x) require(x, character.only = TRUE))))
 quit(save = 'no', status = rr)
